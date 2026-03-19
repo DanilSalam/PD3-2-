ffc95197bbe4face932521493bde5f6a8cb1d96d
DanilSalam <danil2626262626@gmail.com>
2026-03-19 13:04:29 +0200

    Create PowerShell script to log recent warnings

---
+++ 2_zurnals.ps1
@@ -0,0 +1,8 @@
+$logs = Get-WinEvent -LogName Application | Where-Object {
+    $_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -gt (Get-Date).AddDays(-3)
+}
+
+$logs | Out-File "$env:USERPROFILE\Documents\Warnings.txt"
+
+$logs | Group-Object ProviderName | Sort-Object Count -Descending |
+Select-Object -First 3 | Out-File "$env:USERPROFILE\Documents\2_zurnals.ps1"
